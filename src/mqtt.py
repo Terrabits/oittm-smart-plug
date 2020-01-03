69c1e4d72e342da8bf94bb31ecd44dc4f17c3658
Nick Lalic <nick.lalic@gmail.com>
2020-01-02 20:25:04 -0800

    ♻🔥 removing unnecessary concerns.

--- src/mqtt.py
+++ src/mqtt.py
@@ -1,67 +1,70 @@
 from umqtt.simple import MQTTClient
 
 
+def default_subscribe_callback(action):
+    print('mqtt-subscribed action: {0}'.format(action))
+
+
+# subscribe_callback(action)
+# where action is 'on', 'off' or 'toggle'
 class Mqtt:
-    def __init__(self, address, name=b'oittm', main_topic=b'smartplug', handle_on=None, handle_off=None, handle_toggle=None):
+    def __init__(self, address,
+                 name=b'smart-plug', main_topic=b'outlet',
+                 subscribe_callback=default_subscribe_callback):
+        self.address            = address
         self.name               = name
         self.topic              = main_topic
         self.topic_set          = main_topic + b'/set'
         self.topic_toggle       = main_topic + b'/toggle'
-
-        self.address            = address
         self.broker             = None
-
-        self.handle_on          = handle_on
-        self.handle_off         = handle_off
-        self.handle_toggle      = handle_toggle
+        self.subscribe_callback = subscribe_callback
 
     @property
     def connected(self):
         return bool(self.broker)
 
+    # Attempt to connect to mqtt server.
+    # if self.connected: will disconnect current connection before proceeding.
+    # Returns True on success or False otherwise.
     def connect(self):
         self.disconnect()
         self.broker = MQTTClient(self.name, self.address, keepalive=10)
-        try:
-            if self.broker.connect() != 0:
-                return self.connect()
-            self._subscribe()
-            if not self.connected:
-                return self.connect()
-        except OSError as err:
-            print('mqtt.connect exception {0}'.format(err))
-            return self.connect()
+        if self.broker.connect() != 0:
+            # failed
+            self.broker = None
+            return False
+        # succeeded
+        self.broker.set_callback(self._handle_broker_subscription)
+        self.broker.subscribe(self.topic_set)
+        self.broker.subscribe(self.topic_toggle)
+        return True
+
     def disconnect(self):
-        if self.connected:
-            try:
-                self.broker.disconnect()
-            except OSError as err:
-                print('msqtt.disconnect exception {0}'.format(err))
-        self.broker    = None
+        if not self.connected:
+            return
+        self.broker.disconnect()
+        self.broker = None
+
     def publish(self, state):
-        assert self.connected
-        try:
-            return self.broker.publish(self.topic, state, retain=True, qos=1)
-        except OSError:
-            self.connect()
-            return self.publish(state)
+        assert self.connected, "cannot publish: disconnected."
+        return self.broker.publish(self.topic, state, retain=True, qos=1)
+
     def _handle_broker_subscription(self, topic, message):
-        assert self.connected
+        assert self.connected, "cannot respond: disconnected"
         if topic == self.topic_set:
             if message == b'on':
-                self.handle_on()  if self.handle_on     else None
+                self.subscribe_callback('on')
             elif message == b'off':
-                self.handle_off() if self.handle_off    else None
+                self.subscribe_callback('off')
+            else:
+                # unrecognized message
+                pass
         elif topic == self.topic_toggle:
-            self.handle_toggle() if self.handle_toggle  else None
-    def _subscribe(self):
-        assert self.connected
-        self.broker.set_callback(self._handle_broker_subscription)
-        try:
-            self.broker.subscribe(self.topic_set)
-            self.broker.subscribe(self.topic_toggle)
-        except OSError:
-            self.disconnect()
+            self.subscribe_callback('toggle')
+        else:
+            # unrecognized topic
+            pass
+
     def loop(self):
         assert self.connected
         while 1:
